fb3f3a6ce49492e4cac6998bbedf4f6b1c1222bd
Paweł Cejrowski <pcejrowski@gmail.com>
2017-05-27 10:52:42 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
-# download spark sources
-# ./dev/make-distribution.sh --name custom-spark --tgz -Pyarn -Phadoop-2.7 -Dscala-2.10 -DskipTests
 
+whoami=$(whoami)
 
-#SPARK_HOME="$HOME/Software/spark-2.1.1-bin-custom-spark/"
-#TARGET_DIR="/home/wpitula/Studia/sem-3-mgr/wiki-embeddings-bigdl/target/scala-2.10"
-#BASE_DIR="/home/wpitula/Studia/sem-3-mgr/wiki-embeddings-bigdl/datasets"
-
-SPARK_HOME="$HOME/Private/studies/spark/"
-TARGET_DIR="$HOME/Private/studies/distributed-systems/wiki-embeddings/target/scala-2.10"
-BASE_DIR="$HOME/Private/studies/distributed-systems/wiki-embeddings/datasets"
-
+SPARK_HOME="$HOME/Software/spark-2.1.1-bin-custom-spark/"
+if [ "${whoami}" = "wpitula" ]
+then
+    TARGET_DIR="$HOME/Studia/sem-3-mgr/wiki-embeddings-bigdl/target/scala-2.10"
+    BASE_DIR="$HOME/Studia/sem-3-mgr/wiki-embeddings-bigdl/datasets"
+elif [ "${whoami}" = "pcejrowski" ]
+then
+    TARGET_DIR="$HOME/Private/studies/distributed-systems/wiki-embeddings/target/scala-2.10"
+    BASE_DIR="$HOME/Private/studies/distributed-systems/wiki-embeddings/datasets"
+fi
 
 ${SPARK_HOME}/bin/spark-submit \
    --master "local[*]" \
