586eda1bc6f32470ce432b6584bae8a6cb744085
Paweł Cejrowski <pcejrowski@gmail.com>
2017-06-08 01:22:22 +0200

    Uruchomienie

--- run.sh
+++ run.sh
@@ -3,15 +3,16 @@
 set -vx
 whoami=$(whoami)
 
-SPARK_HOME="$HOME/Software/spark-2.1.0-bin-hadoop2.7/"
 if [ "${whoami}" = "wpitula" ]
 then
     TARGET_DIR="$HOME/Studia/sem-3-mgr/wiki-embeddings-bigdl/target/scala-2.11"
     BASE_DIR="$HOME/Studia/sem-3-mgr/wiki-embeddings-bigdl/datasets"
+    SPARK_HOME="$HOME/Software/spark-2.1.0-bin-hadoop2.7/"
 elif [ "${whoami}" = "pcejrowski" ]
 then
     TARGET_DIR="$HOME/Private/studies/distributed-systems/wiki-embeddings/target/scala-2.11"
     BASE_DIR="$HOME/Private/studies/distributed-systems/wiki-embeddings/datasets"
+    SPARK_HOME="$HOME/Software/spark-2.1.0-bin-hadoop2.6/"
 fi
 
 ${SPARK_HOME}/bin/spark-submit \
